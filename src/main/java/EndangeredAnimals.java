3d694dca738df28d556664f3ace4a3603c057a24
Brian Dutz <bgdutz@gmail.com>
2016-09-30 11:35:15 -0700

    start work on the child classes

---
+++ src/main/java/EndangeredAnimals.java
@@ -0,0 +1,19 @@
+import org.sql2o.*;
+import java.util.List;
+
+public class EndangeredAnimals extends Animals {
+  private int health;
+  private int age;
+  public static final String DATABASE_TYPE = "EndangeredAnimals";
+  public final static GOOD_HEALTH = 1;
+  public final static OK_HEALTH = 2;
+  public final static ILL_HEALTH = 3;
+  public final static ADULT_AGE = 1;
+  public final static YOUNG_AGE = 2;
+  public final static NEWBORN-AGE = 3;
+
+
+
+
+
+}
