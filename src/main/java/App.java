149a5c25891c52b8bf6151c44d21a16996880493
Brian Dutz <bgdutz@gmail.com>
2016-10-05 09:21:02 -0700

    reconfigure fornt end again

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -59,17 +59,42 @@ public class App {
 //if else statement to do all animals in one page?
 //add saves needed to save to DB
 
-   post("/sightings", (request, response) -> {
-     Map<String, Object> model = new HashMap<String, Object>();
-     SafeAnimals safeAnimal = SafeAnimals.find(Integer.parseInt(request.params("animalId")));
-     String sightingsLocation = request.queryParams("location");
-     String rangerName = request.queryParams("name");
-     Sightings sighting = new Sightings(sightingsLocation, rangername);
-     sighting.save();
-     model.put("sightings", animals.getAllSightings());
-     model.put("template", "templates/sightings.vtl");
-     return new ModelAndView(model, layout);
-   }, new VelocityTemplateEngine());
+  post("/sightings", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    SafeAnimals safeAnimal = SafeAnimals.find(Integer.parseInt(request.params(":id")));
+    EndangeredAnimals endangeredAnimal = EndangeredAnimals.find(Integer.parseInt(request.params(":id")));
+    String sightingsLocation = request.queryParams("location");
+    String rangerName = request.queryParams("name");
+    Sightings sighting = new Sightings(sightingsLocation, rangerName, safeAnimal.getId());
+    sighting.save();
+    model.put("sightings", Sightings.all());
+    model.put("template", "templates/sightings.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+ // post("/sightings/safeAnimals", (request, response) -> {
+ //   Map<String, Object> model = new HashMap<String, Object>();
+ //   SafeAnimals safeAnimal = SafeAnimals.find(Integer.parseInt(request.params(":id")));
+ //   String sightingsLocation = request.queryParams("location");
+ //   String rangerName = request.queryParams("name");
+ //   Sightings sighting = new Sightings(sightingsLocation, rangerName, safeAnimal.getId());
+ //   sighting.save();
+ //   model.put("sightings", Sightings.all());
+ //   model.put("template", "templates/sightings.vtl");
+ //   return new ModelAndView(model, layout);
+ // }, new VelocityTemplateEngine());
+ //
+ // post("/sightings/endangeredAnimals", (request, response) -> {
+ //   Map<String, Object> model = new HashMap<String, Object>();
+ //   EndangeredAnimals endangeredAnimal = EndangeredAnimals.find(Integer.parseInt(request.params(":id")));
+ //   String sightingsLocation = request.queryParams("location");
+ //   String rangerName = request.queryParams("name");
+ //   Sightings sighting = new Sightings(sightingsLocation, rangerName, endangeredAnimal.getId());
+ //   sighting.save();
+ //   model.put("sightings", Sightings.all());
+ //   model.put("template", "templates/sightings.vtl");
+ //   return new ModelAndView(model, layout);
+ // }, new VelocityTemplateEngine());
 
   //  post("/sightings/endangeredAnimal", (request, response) -> {
   //    Map<String, Object> model = new HashMap<String, Object>();
